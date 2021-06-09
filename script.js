0556454b008711473486f083176c8e680ad96a3c
Anoop G <mr.anoopg@gmail.com>
2021-06-10 00:41:32 +0530

    Model update and logic updated for less input

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let inputData;
 
 async function preload() {
     console.log('Fetching Data');
-    $.getJSON("./data.json", async function (data) {
-        await formatData(data.results)
+    $.getJSON("./fifadata.json", async function (data) {
+        await formatData(data)
     });
 }
 
@@ -16,17 +16,16 @@ async function formatData(data) {
     let playerRecord = [];
     let playerLabelTens = [];
     for (let i = 0; i < 3000; i++) {
-        let preferedFoot = data[i]['Preferred Foot'] == 'Right' ? 1 : 0;
-        let players = [preferedFoot, data[i].Crossing, data[i].Finishing, data[i].HeadingAccuracy, data[i].ShortPassing, data[i].Volleys, data[i].Dribbling, data[i].Curve, data[i].FKAccuracy, data[i].LongPassing, data[i].BallControl, data[i].Acceleration, data[i].SprintSpeed, data[i].Reactions, data[i].Balance, data[i].ShotPower, data[i].Jumping, data[i].Stamina, data[i].Strength, data[i].Agility, data[i].LongShots, data[i].Aggression, data[i].Interceptions, data[i].Positioning, data[i].Vision, data[i].Penalties, data[i].Composure, data[i].Marking, data[i].StandingTackle, data[i].SlidingTackle, data[i].GKDiving, data[i].GKHandling, data[i].GKKicking, data[i].GKPositioning, data[i].GKReflexes];
+        let players = [data[i].prefered_foot, data[i].ballskills_and_passing,data[i].defense_and_mental,data[i].physical,data[i].shooting,data[i].gk_ability];
         playerRecord.push(players);
         playerLabel.push({
-            'name': data[i].Name,
-            'club': data[i].Club,
-            'age': data[i].Age,
-            'overall': data[i].Overall,
-            'potential': data[i].Potential,
-            'position': data[i].Position,
-            'pf': data[i]['Preferred Foot']
+            'name': data[i].name,
+            'club': data[i].club,
+            'age': data[i].age,
+            'overall': data[i].overall,
+            'potential': data[i].potential,
+            'position': data[i].position,
+            'pf': data[i]['pf']
         });
         //posLabel.push(data[i].Position);
         playerLabelTens.push(i)
@@ -37,16 +36,16 @@ async function formatData(data) {
     ys = tf.oneHot(labelTensor, 3000);
     labelTensor.dispose();
     // xs.print();
-    // ys.print();
+    //ys.print();
 
     model = tf.sequential();
     let hidden = tf.layers.dense({
-        units: 10,
+        units: 100,
         activation: 'relu',
-        inputDim: 35
+        inputDim: 6
     });
     let hidden2 = tf.layers.dense({
-        units: 20,
+        units: 200,
         useBias: true,
         activation: 'relu'
     });
@@ -62,7 +61,7 @@ async function formatData(data) {
     //create optimiser
 
     const lr = 0.2;
-    const optimizer = tf.train.adam()
+    const optimizer = tf.train.adam();
 
     model.compile({
         optimizer: optimizer,
@@ -114,42 +113,43 @@ async function test() {
 }
 
 async function process() {
-    inputData = [parseInt(document.forms["football"]["pfin"].value),
-        parseInt(document.forms["football"]["crossing"].value),
-        parseInt(document.forms["football"]["finishing"].value),
-        parseInt(document.forms["football"]["headingaccuracy"].value),
-        parseInt(document.forms["football"]["shortpassing"].value),
-        parseInt(document.forms["football"]["volleys"].value),
-        parseInt(document.forms["football"]["dribbling"].value),
-        parseInt(document.forms["football"]["curve"].value),
-        parseInt(document.forms["football"]["fkaccuracy"].value),
-        parseInt(document.forms["football"]["longpassing"].value),
-        parseInt(document.forms["football"]["ballcontrol"].value),
-        parseInt(document.forms["football"]["acceleration"].value),
-        parseInt(document.forms["football"]["sprintspeed"].value),
-        parseInt(document.forms["football"]["agility"].value),
-        parseInt(document.forms["football"]["reactions"].value),
-        parseInt(document.forms["football"]["balance"].value),
-        parseInt(document.forms["football"]["shotpower"].value),
-        parseInt(document.forms["football"]["jumping"].value),
-        parseInt(document.forms["football"]["stamina"].value),
-        parseInt(document.forms["football"]["strength"].value),
-        parseInt(document.forms["football"]["longshots"].value),
-        parseInt(document.forms["football"]["aggression"].value),
-        parseInt(document.forms["football"]["interceptions"].value),
-        parseInt(document.forms["football"]["positioning"].value),
-        parseInt(document.forms["football"]["vision"].value),
-        parseInt(document.forms["football"]["penalties"].value),
-        parseInt(document.forms["football"]["composure"].value),
-        parseInt(document.forms["football"]["marking"].value),
-        parseInt(document.forms["football"]["standingtackle"].value),
-        parseInt(document.forms["football"]["slidingtackle"].value),
-        parseInt(document.forms["football"]["gkdiving"].value),
-        parseInt(document.forms["football"]["gkhandling"].value),
-        parseInt(document.forms["football"]["gkkicking"].value),
-        parseInt(document.forms["football"]["gkpositioning"].value),
-        parseInt(document.forms["football"]["gkreflexes"].value)
-    ]
+    inputData = [parseInt(document.forms["football"]["pfin"].value),parseInt(document.forms["football"]["bsp"].value)/100,parseInt(document.forms["football"]["dfm"].value)/100,parseInt(document.forms["football"]["physical"].value)/100,parseInt(document.forms["football"]["shooting"].value)/100,parseInt(document.forms["football"]["gkability"].value)/100]
+    // // [parseInt(document.forms["football"]["pfin"].value),
+    // //     parseInt(document.forms["football"]["crossing"].value),
+    // //     parseInt(document.forms["football"]["finishing"].value),
+    // //     parseInt(document.forms["football"]["headingaccuracy"].value),
+    // //     parseInt(document.forms["football"]["shortpassing"].value),
+    // //     parseInt(document.forms["football"]["volleys"].value),
+    // //     parseInt(document.forms["football"]["dribbling"].value),
+    // //     parseInt(document.forms["football"]["curve"].value),
+    // //     parseInt(document.forms["football"]["fkaccuracy"].value),
+    // //     parseInt(document.forms["football"]["longpassing"].value),
+    // //     parseInt(document.forms["football"]["ballcontrol"].value),
+    // //     parseInt(document.forms["football"]["acceleration"].value),
+    // //     parseInt(document.forms["football"]["sprintspeed"].value),
+    // //     parseInt(document.forms["football"]["agility"].value),
+    // //     parseInt(document.forms["football"]["reactions"].value),
+    // //     parseInt(document.forms["football"]["balance"].value),
+    // //     parseInt(document.forms["football"]["shotpower"].value),
+    // //     parseInt(document.forms["football"]["jumping"].value),
+    // //     parseInt(document.forms["football"]["stamina"].value),
+    // //     parseInt(document.forms["football"]["strength"].value),
+    // //     parseInt(document.forms["football"]["longshots"].value),
+    // //     parseInt(document.forms["football"]["aggression"].value),
+    // //     parseInt(document.forms["football"]["interceptions"].value),
+    // //     parseInt(document.forms["football"]["positioning"].value),
+    // //     parseInt(document.forms["football"]["vision"].value),
+    // //     parseInt(document.forms["football"]["penalties"].value),
+    // //     parseInt(document.forms["football"]["composure"].value),
+    // //     parseInt(document.forms["football"]["marking"].value),
+    // //     parseInt(document.forms["football"]["standingtackle"].value),
+    // //     parseInt(document.forms["football"]["slidingtackle"].value),
+    // //     parseInt(document.forms["football"]["gkdiving"].value),
+    // //     parseInt(document.forms["football"]["gkhandling"].value),
+    // //     parseInt(document.forms["football"]["gkkicking"].value),
+    // //     parseInt(document.forms["football"]["gkpositioning"].value),
+    // //     parseInt(document.forms["football"]["gkreflexes"].value)
+    // // ]
     document.getElementById("plyername").innerHTML = "";
     document.getElementById("age").innerHTML = "";
     document.getElementById("position").innerHTML = "";
