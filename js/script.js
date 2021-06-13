0fbde6e966d01e92b55bf04a4c37e6cdede12715
Anoop G <mr.anoopg@gmail.com>
2021-06-13 12:11:25 +0530

    input validation added

--- js/script.js
+++ js/script.js
@@ -166,3 +166,12 @@ async function process() {
     document.documentElement.scrollTop = 0;
     await preload();
 }
+
+function limit(element)
+{
+    var max_chars = 2;
+
+    if(element.value.length > max_chars) {
+        element.value = element.value.substr(0, max_chars);
+    }
+}
